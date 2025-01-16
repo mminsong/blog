015c442b85e2484b5e3c86907282a2f30ff78ebf
mminsong <pamten@naver.com>
2025-01-16 15:22:00 +0900

    강의 끝

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -11,8 +13,14 @@ function App() {
     return 100
   }
 
-  let [글제목, 글제목변경] = useState(['남자코트추천','여자코트추천']);
-
+  let [글제목, 글제목변경] = useState(['aaa','bbb','ccc']);
+  function 제목변경(){
+      var newArray =[...글제목];
+      newArray[0]='여자코트 추천';
+      글제목변경(newArray);
+      //글제목변경(['AAA','bbb','ccc']);
+  }
+  let [숫자,숫자변경] = useState(0);
   return (
     <div className="App">
       <div className="black-nav">
@@ -26,12 +34,38 @@ function App() {
         <hr/>
       </div>
       <div className="list">
-        <h3>{글제목[0]}</h3>
+        <h3>{글제목[0]} <span onClick={()=>{숫자변경(숫자+1)}}>👍</span>{숫자}</h3>
+        <button onClick={ 제목변경}>버튼</button>
         <p>2월 17일 발행</p>
         <hr/>
       </div>
+      <div className="list">
+        <h3>{글제목[1]}</h3>
+        <p>2월 18일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3>{글제목[2]}</h3>
+        <p>2월 19일 발행</p>
+        <hr/>
+      </div>
+      
+      <div className="modal">
+        <h2>제목</h2>
+        <p>날짜</p>
+        <p>상세내용</p>
+      </div>
+
+      <Modal></Modal>
     </div>
   );
+}
+
+//Component 만드는 법
+function Modal(){
+  return(
+    <div>1</div>
+  )
 }
 
 export default App;
